b64758ae2e1bc3ac77a79b432d693de200bfd2fd
realbrodiwhite <brodiwhite951@gmail.com>
2024-06-25 20:04:52 -0600

    updates

--- valleyroofmedics/tailwind.config.js
+++ valleyroofmedics/tailwind.config.js
@@ -1,7 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
